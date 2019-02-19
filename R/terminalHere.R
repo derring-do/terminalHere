0ce6c3c3fe2ccc28ee66e5c9453eac6e66afdf58
derring-do <5824530+derring-do@users.noreply.github.com>
2019-02-19 12:25:28 -0500

    Add files via upload

---
+++ R/terminalHere.R
@@ -0,0 +1,11 @@
+#' Open RStudio Terminal in active file location
+#'
+#' @return
+#' @import rstudioapi
+#' @export
+#'
+#' @examples
+terminalHere <- function() {
+  termId <- terminalCreate(caption = dirname(getSourceEditorContext()$path))
+  terminalSend(id = termId, text = paste("cd ", dirname(getSourceEditorContext()$path)))
+}
