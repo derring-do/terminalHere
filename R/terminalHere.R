fa9c4e411544ca65a977e3add00559587d9d9a48
derring-do <5824530+derring-do@users.noreply.github.com>
2019-02-19 12:57:42 -0500

    remove space

--- R/terminalHere.R
+++ R/terminalHere.R
@@ -7,5 +7,5 @@
 #' @examples
 terminalHere <- function() {
   termId <- terminalCreate(caption = dirname(getSourceEditorContext()$path))
-  terminalSend(id = termId, text = paste("cd ", dirname(getSourceEditorContext()$path)))
+  terminalSend(id = termId, text = paste("cd", dirname(getSourceEditorContext()$path)))
 }
